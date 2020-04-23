cd98a4aa4e81c5a30d7e83cea307a2d2ff4fd632
Erick Suarez <ericksuarezdev@gmail.com>
2020-04-22 22:32:55 -0500

    retry

--- src/test/java/dev/ericksuarez/demo/service/ServiceFakeTest.java
+++ src/test/java/dev/ericksuarez/demo/service/ServiceFakeTest.java
@@ -20,7 +20,7 @@ public class ServiceFakeTest {
 
     @Test
     public void foo() {
-        assertEquals(serviceFake.getEnv(), "dev test null");
+        assertEquals(serviceFake.getEnv(), "dev test x null");
     }
 
     @Test
